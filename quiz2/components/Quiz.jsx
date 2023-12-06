52bd9ef2875fd896127d42faa2b7fa37f2e41628
vedha <vedh1305@gmail.com>
2023-12-06 15:51:23 +0530

    Update

---
+++ quiz2/components/Quiz.jsx
@@ -0,0 +1,58 @@
+import { Component } from 'react'
+import QuestionData from '../resources/quizQuestion.json'
+
+export default class Quiz extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         index:0,
+         questions:QuestionData
+      }
+    }
+
+    handlePrevious = () =>{
+        this.setState((prevState) => ({ index:prevState.index-1}))
+    }
+
+    handleNext = () => {
+        this.setState((prevState) => ({index:prevState.index+1}))
+    }
+
+    handleQuit = () =>{
+        const confirm =window.confirm("Are you sure you want to quit?")
+        confirm?this.setState(({index:0})):null
+    }
+
+  render() {
+    const { index , questions } = this.state;
+    const currentQuestion = questions[index];
+    const totalQuestions = questions.length;
+
+    return (
+      <>
+       <div id="quiz">
+        <div id='container'>
+            <h1>Question</h1>
+            <h4>{index+1} of {totalQuestions}</h4>
+            <p>{currentQuestion.question}</p>
+            <div id="optionA">
+            <button>{currentQuestion.optionA}</button>
+            <button>{currentQuestion.optionB}</button>
+            </div>
+            <div id="optionB">
+            <button>{currentQuestion.optionC}</button>
+            <button>{currentQuestion.optionD}</button>
+            </div>
+            <div id="select">
+                <button id="prev" onClick={this.handlePrevious} disabled={index==0}>Previous</button>
+                <button id="next" onClick={this.handleNext} disabled={index==totalQuestions-1}>Next</button>
+                <button id="quit" onClick={this.handleQuit}>Quit</button>
+            </div>
+        </div>
+        </div>
+      </>
+    )
+  }
+}
---
+++ quiz2/src/App.jsx
@@ -0,0 +1,13 @@
+import Quiz from '../components/Quiz'
+import './App.css'
+import { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Quiz/>
+      </div>
+    )
+  }
+}
